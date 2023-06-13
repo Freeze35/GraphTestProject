39ee80833c918b2ef9694a43a1f5b7863de0e6d4
Freeze35 <HumanSolidarity@protonmail.com>
2023-06-13 08:02:06 +0800

    Adding test pager and pass data set

---
+++ README.md
@@ -0,0 +1,7 @@
+
+### Project for creatiing visualization graphs or bars using Kotlin Library [vico](https://github.com/patrykandpatrick/vico)  
+
+Inside Main our central project Bar graph with data  
+[Main](https://github.com/Freeze35/GraphTestProject/blob/f8beaffcac0d485f3747061dc6052609932af570/app/src/main/java/com/example/graphproject/MainActivity.kt)  
+
+[Link on HorizontalPager](https://www.youtube.com/watch?v=ro3a-GmaLqE&ab_channel=PhilippLackner)
--- app/src/main/java/com/example/graphproject/MainActivity.kt
+++ app/src/main/java/com/example/graphproject/MainActivity.kt
@@ -3,20 +3,36 @@ package com.example.graphproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import com.example.graphproject.ui.theme.GraphProjectTheme
 import com.example.graphproject.ui.theme.previews.utils.VicoTheme
 import com.patrykandpatrick.vico.compose.axis.horizontal.bottomAxis
@@ -28,11 +44,12 @@ import com.patrykandpatrick.vico.compose.component.shape.shader.verticalGradient
 import com.patrykandpatrick.vico.compose.component.shapeComponent
 import com.patrykandpatrick.vico.compose.component.textComponent
 import com.patrykandpatrick.vico.compose.dimensions.dimensionsOf
+import com.patrykandpatrick.vico.core.axis.AxisPosition
+import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
 import com.patrykandpatrick.vico.core.axis.vertical.createVerticalAxis
 import com.patrykandpatrick.vico.core.component.shape.LineComponent
 import com.patrykandpatrick.vico.core.entry.entriesOf
 import com.patrykandpatrick.vico.core.entry.entryModelOf
-
 
 
 class MainActivity : ComponentActivity() {
@@ -42,7 +59,7 @@ class MainActivity : ComponentActivity() {
             GraphProjectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.height(200.dp),
+                    modifier = Modifier
                 ) {
                     DoubleBar()
                 }
@@ -51,63 +68,209 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-private val chartModifier = Modifier.height(300.dp)
+
 val positiveColor: Color = Color(0xFF48FF85)
 val positiveColor2: Color = Color(0xFF00FCD6)
 val negativeColor: Color = Color(0xFFF5A6B5)
 val negativeColor2: Color = Color(0xFFE40123)
 val axisNumericalColor: Color = Color(0xFF48FF85)
 
+//Class for our text field
+data class HeadData(
+    var name: String,
+    var value: String,
+)
 
+//Data for showing many text and set them numbers(or text)
+private val textNameValue = mutableListOf(
+    HeadData("За месяц", "24"),
+    HeadData("По клиенту", "44"),
+    HeadData("По точке", "34")
+)
 
+var EntryData = entryModelOf(
+    entriesOf(3, 2, 2, 3, 1, 3, 2, 2, 3, 1),
+    entriesOf(1, 3, 1, 2, 3, 1, 3, 1, 2, 3)
+)
+
+private val axisValueFormatter = AxisValueFormatter<AxisPosition.Horizontal.Bottom> { i, _ ->
+    if ( i< 10) {
+        "0${i+1}.02.2023"
+
+    }
+    else {
+        "${i+1}.02.2023"
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
 @Preview
 @Composable
 fun DoubleBar(): Unit = VicoTheme {
-        Chart(
-            modifier = chartModifier,
-            chart = columnChart(
-                columns = listOf(
-                    lineComponent(
-                        thickness = 8.dp, //size bar
-                        shape = RoundedCornerShape(4.dp), //round bar
-                        dynamicShader = verticalGradient(arrayOf(negativeColor, negativeColor2)), // colors for bar
-                    ),
-                    lineComponent(
-                        thickness = 8.dp, //size bar
-                        shape = RoundedCornerShape(4.dp), //round bar
-                        dynamicShader = verticalGradient(arrayOf(positiveColor, positiveColor2)), // colors for bar
-                    ),
-                ),
-            ),
-            endAxis = createVerticalAxis {
-                maxLabelCount = 8 // number of indicators on the right or left
-                label = textComponent(
-                    color = axisNumericalColor,
-                    textSize = 10.sp,
-                    background = shapeComponent(
-                        shape = CutCornerShape(
-                            CornerSize(percent = 25),
-                            CornerSize(percent = 50),
-                            CornerSize(percent = 50),
-                            CornerSize(percent = 25),
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+
+        val pagerState = rememberPagerState()
+        HorizontalPager(
+            pageCount = 2,//set 2 pages chart and Table
+            state = pagerState,
+            key = { it },
+            modifier = Modifier.fillMaxHeight(0.4f)
+        ) {index ->
+            // Our order chart
+            if (index == 0){
+                Chart(
+                    modifier = Modifier
+                        .fillMaxHeight(0.4f)
+                        .zIndex(1f)
+                    , // set height chart 40%
+                    chart = columnChart(
+                        columns = listOf(
+                            lineComponent(
+                                thickness = 8.dp, //size bar
+                                shape = RoundedCornerShape(4.dp), //round bar
+                                dynamicShader = verticalGradient(
+                                    arrayOf(
+                                        negativeColor,
+                                        negativeColor2
+                                    )
+                                ), // colors for bar
+                            ),
+                            lineComponent(
+                                thickness = 8.dp, //size bar
+                                shape = RoundedCornerShape(4.dp), //round bar
+                                dynamicShader = verticalGradient(
+                                    arrayOf(
+                                        positiveColor,
+                                        positiveColor2
+                                    )
+                                ), // colors for bar
+                            ),
                         ),
-                        color = colors.primary.copy(alpha = 0.1f),
                     ),
-                    padding = dimensionsOf(end = 8.dp, start =4.dp),
+                    endAxis = createVerticalAxis {
+                        maxLabelCount = 8 // number of indicators on the right or left
+                        label = textComponent(
+                            color = axisNumericalColor,
+                            textSize = 10.sp,
+                            background = shapeComponent(
+                                shape = CutCornerShape(  //change shaps one bar
+                                    CornerSize(percent = 25),
+                                    CornerSize(percent = 50),
+                                    CornerSize(percent = 50),
+                                    CornerSize(percent = 25),
+                                ),
+                                color = colors.primary.copy(alpha = 0.1f),
+                            ),
+                            padding = dimensionsOf(end = 8.dp, start = 4.dp),
+                        )
+                        axis = null
+                        tick = null
+                        guideline = LineComponent(
+                            axisNumericalColor.copy(alpha = 0.1f).toArgb(), // color lines indicators
+                            1.dp.value,
+                        )
+                    },
+                    bottomAxis = bottomAxis(  // down numbers can change in date
+                        label = textComponent(
+                            color = axisNumericalColor,
+                            textSize = 10.sp,
+                        ),
+                        valueFormatter = axisValueFormatter // See above for the description of the bottom axle
+                    ),
+                    model = @Suppress("MagicNumber") (EntryData), // our data from poutside for graph
+                    chartScrollSpec = rememberChartScrollSpec(isScrollEnabled = true),
                 )
-                axis = null
-                tick = null
-                guideline = LineComponent(
-                    axisNumericalColor.copy(alpha = 0.1f).toArgb(), // color lines indicators
-                    1.dp.value,
-                )
-            },
-            bottomAxis = bottomAxis(
-                label = textComponent(
-                color = axisNumericalColor,)
-            ),
-            model = @Suppress("MagicNumber") (entryModelOf(entriesOf(3, 2, 2, 3, 1,3, 2, 2, 3, 1), entriesOf(1, 3, 1, 2, 3,1, 3, 1, 2, 3))),
-            chartScrollSpec = rememberChartScrollSpec(isScrollEnabled = true),
+            }
+            else{
+                Row(
+                    modifier = Modifier.fillMaxHeight(0.4f).zIndex(1f)
+                ) {
+                    Text(
+                        text = "TWEDSDSADd",
+                        textAlign = TextAlign.Center,
+                        maxLines = 1,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = Color.Gray
+                    )
+                }
+            }
+
+        }
+
+        //Text Data
+        //Inside Rows (LazyRow( Column : 2Element))
+        Row(modifier = Modifier
+            .fillMaxWidth()
+
+        )
+        {
+            LazyRow(
+                modifier = Modifier.fillMaxWidth(0.5f),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                itemsIndexed(textNameValue) { _, text ->
+                    Column(
+                        modifier = Modifier
+                            .padding(horizontal = 2.dp, vertical = 2.dp)
+                    ) {
+                        Text(
+                            text = text.name,
+                            textAlign = TextAlign.Center,
+                            maxLines = 1,
+                            style = MaterialTheme.typography.bodySmall,
+                            color = Color.Gray
+                        )
+                        Text(
+                            text = text.value + " шт",
+                            textAlign = TextAlign.Center,
+                            maxLines = 1,
+                            style = MaterialTheme.typography.bodySmall,
+                            color = Color.Gray
+                        )
+                    }
+
+                }
+            }
+            //Split Line
+            Divider(
+                color = Color.Black,
+                modifier = Modifier //fill the max height
+                    .width(1.dp)
+                    .padding(top = 7.dp) //padding top
+                    .height(26.dp) // height divider
             )
 
+            //Right Side
+            LazyRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                itemsIndexed(textNameValue) { _, text ->
+                    Column(
+                        modifier = Modifier
+                            .padding(horizontal = 2.dp, vertical = 2.dp)
+                    ) {
+                        Text(
+                            text = text.name,
+                            textAlign = TextAlign.Center,
+                            maxLines = 1,
+                            style = MaterialTheme.typography.bodySmall,
+                            color = Color.Gray
+                        )
+                        Text(
+                            text = text.value + " шт",
+                            textAlign = TextAlign.Center,
+                            maxLines = 1,
+                            style = MaterialTheme.typography.bodySmall,
+                            color = Color.Gray
+                        )
+                    }
+
+                }
+            }
+        }
+
+
+    }
 }
