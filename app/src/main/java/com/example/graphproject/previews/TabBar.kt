c5c485a2fcf9932f9fe033e159ae5a817e282429
elshin.p <petakioto2.0@gmail.com>
2023-06-23 19:14:38 +0800

    Add new LazyHorizontalPager

--- app/src/main/java/com/example/graphproject/previews/TabBar.kt
+++ app/src/main/java/com/example/graphproject/previews/TabBar.kt
@@ -7,13 +7,16 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ScrollableTabRow
@@ -21,29 +24,35 @@ import androidx.compose.material.Tab
 import androidx.compose.material.TabPosition
 import androidx.compose.material.TabRowDefaults
 import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
 import androidx.compose.ui.zIndex
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.calculateCurrentOffsetForPage
 import com.google.accompanist.pager.pagerTabIndicatorOffset
 import com.google.accompanist.pager.rememberPagerState
 import kotlinx.coroutines.launch
+import kotlin.math.absoluteValue
 
 @Preview
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun TabBar() {
-    val pagerState = rememberPagerState()
+    val pagerState = rememberPagerState(1)
     val coroutineScope = rememberCoroutineScope()
-    val pages = listOf("kotlin","java","c#","php","golang","rust")
+    val pages = listOf("1971","1972","1973#","1974","1975","1976")
 
     val defaultIndicator = @Composable { tabPositions: List<TabPosition> ->
         TabRowDefaults.Indicator(
@@ -57,7 +66,9 @@ fun TabBar() {
     //for fixed position size use TabRow
     //for many values better ScrollableTabRow
     ScrollableTabRow(
-        modifier = Modifier.height(50.dp).zIndex(2f), // set necessary for change tab. not forget
+        modifier = Modifier
+            .height(50.dp)
+            .zIndex(2f), // set necessary for change tab. not forget
         selectedTabIndex = pagerState.currentPage,
         indicator = indicator
     ) {
@@ -93,11 +104,59 @@ fun TabBar() {
     }
 
     HorizontalPager(
-        count = 11,
+        modifier = Modifier.fillMaxWidth(),
+        count = pages.size,
         state = pagerState,
-    ) { page ->
-        Box(Modifier.fillMaxSize()) {
-            Text(modifier = Modifier.align(Alignment.Center), text = "Page $page")
+    ) { id ->
+        Box(Modifier.fillMaxSize(),
+
+        ) {
+            Text(modifier = Modifier.align(Alignment.Center), text = "Page ${pages[id]}")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LazyHorizontalPager (){
+    HorizontalPager(count = 4) { page ->
+        Column(
+            Modifier
+                .graphicsLayer {
+                    // Calculate the absolute offset for the current page from the
+                    // scroll position. We use the absolute value which allows us to mirror
+                    // any effects for both directions
+                    val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+
+                    // We animate the scaleX + scaleY, between 85% and 100%
+                    lerp(
+                        start = 0.5f,
+                        stop = 1f,
+                        fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                    ).also { scale ->
+                        scaleX = scale
+                        scaleY = scale
+                    }
+
+                    // We animate the alpha, between 50% and 100%
+                    alpha = lerp(
+                        start = 0.5f,
+                        stop = 1f,
+                        fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                    )
+                }
+
+            ,verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier.widthIn(max = 120.dp),
+                text = "Дата",
+                textAlign = TextAlign.Center,
+
+                maxLines = 1,
+                style = MaterialTheme.typography.bodySmall,
+                color = Color.Black)
         }
     }
 }
